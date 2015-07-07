7943d091be6e4a75ba549b72a1d277aba7f54a27
Stanislaw Datskevich <me@nek0.net>
2015-07-07 11:55:15 +0300

    Added first version

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # php_mail_copyenv
+
+Idea has been got from this page: http://wiki.jarfil.net/Php_mail_copyenv#Simple_wrapper
+
+Scripts should be reworked and tested.
---
+++ src/set_php_headers.php
@@ -0,0 +1,5 @@
+<?php
+putenv("HTTP_HOST=". $_SERVER["HTTP_HOST"]);
+putenv("SCRIPT_NAME=". $_SERVER["SCRIPT_NAME"]);
+putenv("REMOTE_ADDR=". $_SERVER["REMOTE_ADDR"]);
+?>
